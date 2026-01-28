dd97a7bf6f9354cb192d33874d0dcde15bcbd4d5
htldacosta <hitaludcosta@gmail.com>
2026-01-28 10:55:03 -0400

    business was finalized

--- src/main/java/com/crud_11/alunoscrud/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/crud_11/alunoscrud/infrastructure/repository/UsuarioRepository.java
@@ -3,7 +3,15 @@ package com.crud_11.alunoscrud.infrastructure.repository;
 import com.crud_11.alunoscrud.infrastructure.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository <Usuario, Integer>{
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
