fadf817f5b5b1fa010b725d7242c4bfeb5d97aa1
htldacosta <hitaludcosta@gmail.com>
2026-01-28 09:47:33 -0400

    first commit

---
+++ src/main/java/com/crud_11/alunoscrud/AlunoscrudApplication.java
@@ -0,0 +1,13 @@
+package com.crud_11.alunoscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoscrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoscrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud_11/alunoscrud/AlunoscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_11.alunoscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
